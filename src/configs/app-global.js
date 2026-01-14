324446f39f4b1a56ed86d4a5f24e7b16a3a2a559
Oliver Kandagor <kandagoroliver2@gmail.com>
2026-01-14 13:12:47 +0300

    Update RECAPTCHASITEKEY in app-global.js

--- src/configs/app-global.js
+++ src/configs/app-global.js
@@ -24,7 +24,7 @@ export const APP_ID = '1:337727804918:web:e8f94fded602473d0ee3d7';
 export const MEASUREMENT_ID = 'G-L6Z548FGKX"';
 
 // recaptcha key, you can get it from https://www.google.com/recaptcha/admin/create
-export const RECAPTCHASITEKEY = '6LeMQEosAAAAAKlIHZjFb-rNCAgdAvj7Ghbv9zxb';
+export const RECAPTCHASITEKEY = '6LeMQEosAAAAAPgokIQWVGZzKIftQy_PRK1ghD8H';
 
 // demo data, no need to change
 export const LAT = 47.4143302506288;
